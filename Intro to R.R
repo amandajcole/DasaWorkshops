bafce1b525b685ba2ab96c7e6d1dc2afe06ed3ff
amandajcole <amandaj3038@gmail.com>
2021-10-28 19:44:22 +1100

    ggplot2 intro workshop

--- Intro to R.R
+++ Intro to R.R
@@ -1,18 +1,26 @@
 # install.packages("tidyverse")
+#install.packages('knitr')
+#install.packages("janitor")
 library(tidyverse)
-
+library(ggplot2)
+library(knitr)
+library(janitor)
 # CTRL + SHIFT + C to comment multiple lines (Windows)
 
-#We are going to work with the Starwars data set that is inbuilt in R
+#We are going to work with the Starwars data set that is inbuilt in R, this is a relatively clean data set with not many missing values
+#you will learn some base functions that will start you on the road to becoming a data-jedi
+
+
 
 view(starwars)
 
 #Assign the dataset starwars to the dataframe "data"
+str(starwars)
 
+#Assign the first 11 columns to dataframe "data"
+data <- select(starwars, c(1:11)) 
 
-data <- starwars
-
-#now the data set shows up in the environment with 87 observations of 14 variables
+#now the data set shows up in the environment with 87 observations of 11 variables
 
 view(data)
 
@@ -27,19 +35,22 @@ filter()
 slice()
 arrange()
 
-# The pipe function %>% to combine nested functions into one section of code
+data %>% count(species, homeworld, sort = TRUE)
+
+
+data %>% count(homeworld, species, sort = TRUE)
+
+data %>% count(species, eye_color, sort = TRUE)
+data %>% count(eye_color, species, sort = TRUE)
 
 filter(data, homeworld == "Tatooine")
 
 # This shows a tibble with the 10 characters that were from Tatooine but has no effect on the original dataset
 
 
-filter(data, homeworld == "Tatooine", sex == "male")
+filter(data, homeworld == "Tatooine", sex == "male") 
 
 # This shows a tibble with the 6 male characters that were from Tatooine but has no effect on the original dataset
-
-
-
 
 
 # we can assign these observations to their own dataframe
@@ -49,13 +60,26 @@ Tatooine <- filter(data, homeworld == "Tatooine")
 male_tatooine <- filter(data, homeworld == "Tatooine", sex == "male")
 
 # Or using the "pipe" function we can achieve a similar outcome:
-
+# The pipe function %>% to combine nested functions into one section of code
 female_characters <- data %>%
   filter(sex == "female") %>%
   arrange(homeworld)
 
-data %>%
-  filter(species == "Human" & films =="Attack of the Clones" )
+kable(female_characters)
+
+
+test<- starwars %>%
+ select(name, species,sex, homeworld, films) %>%
+  filter(species == "Droid" & films =="The Empire Strikes Back" ) %>%
+  arrange(name)
+
+test2<- starwars %>%
+  select(name, species,sex, homeworld, films) %>%
+  filter(species == "Droid") %>%
+  arrange(name)
+
+str(starwars)
+class(starwars$films)
 
 
 # lets look at the tallest characters
@@ -68,6 +92,7 @@ data %>% slice_max(mass, n=10)
 
 # Notice that IG-88 is a Droid with NA as a homeworld - is this the case for all droids?
 
+slice_min()
 droid <- data %>%
  filter(species == "Droid") %>%
   select(name, height, mass, homeworld) %>%
@@ -88,8 +113,7 @@ data2 <- data %>%
 
 # Lets look at the BMI for each of the characters and assign it to a new dataset
 data3 <- data2 %>% mutate(
-  BMI = mass/ (height_m^2)
-) %>%
+  BMI = mass/ (height_m^2)) %>%
   select(BMI, everything())
 
 #lets deal with the missing values
@@ -98,3 +122,53 @@ data3 <- data2 %>%
   filter(!is.na(mass)) %>%
   mutate(BMI = mass/ (height_m^2)) %>%
   select(BMI, everything())
+
+#
+ggplot(starwars, aes(species)) +
+  geom_bar() +
+  coord_flip()
+
+ggplot(starwars, aes(homeworld)) +
+  geom_bar() +
+  coord_flip()
+
+human <-data %>%
+  filter(species == "Human" & homeworld == "Naboo" | homeworld == "Tatooine")
+
+ggplot(human, aes(homeworld)) +
+  geom_bar()+
+  coord_flip()
+
+ggplot(starwars, aes(homeworld, fill = gender)) +
+  geom_bar() +
+  coord_flip()
+
+starwars %>% 
+  filter(!is.na(homeworld)) %>% 
+  ggplot(aes(homeworld, fill=gender))+
+  geom_bar() +
+  coord_flip()
+
+starwars %>% 
+  filter(!is.na(homeworld)) %>% 
+  ggplot(aes(homeworld, fill=gender))+
+  geom_bar(position='stack') +
+  coord_flip()
+  
+
+#using the janitor package
+
+tabyl(starwars, homeworld)
+
+# remove empty rows
+clean_data <-remove_empty(starwars, which = c("rows", "cols"))
+
+tabyl(clean_data, homeworld)  
+
+# we still have the NA's, theres no completely empty rows or columns
+
+# cross tablulation
+clean_data %>% filter(sex != "female") %>%
+  tabyl(height,species)
+
+clean_data %>% tabyl(height,gender)
--- Plastics.R
+++ Plastics.R
@@ -3,8 +3,14 @@
 library(tidyverse)
 library(skimr)
 library(ggplot2)
+#library(janitor)
 
-# The original dataset come from Break free from Plastic and was put together by Sarah Sauve. This was featured in Tidy Tuesday in January 2021.
+
+# The original dataset come from Break free from Plastic and was put together by 
+#Sarah Sauve. This was featured in Tidy Tuesday in January 2021.
+
+# https://github.com/rfordatascience/tidytuesday/blob/master/data/2021/2021-01-26/readme.md
+
 
 # read in the dataset
 plastics <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-26/plastics.csv')
@@ -12,13 +18,17 @@ plastics <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytues
 skim(plastics)
 
 
-# There are two columns as character and the rest are numeric, looking at the data there are some missing variables
-# and we need to work out how to use them. Do we replace them with zeros, or ignore them in the analysis. That depends on what question we are asking.
+# There are two columns as character and the rest are numeric, looking at the data
+# there are some missing variables
+# and we need to work out how to use them. 
+# Do we replace them with zeros, or ignore them in the analysis. 
+# That depends on what question we are asking.
 
 
 rename(plastics, other=o)
 
 view(plastics)
+
 
 plastics<-rename(plastics, other = o)
 view(plastics)
@@ -40,6 +50,14 @@ view(oz)
 count_country <- plastics %>%
   group_by(year) %>%
   count(country)
+
+# Count the number of events
+count_country %>% count(year)
+# add the filter for Australia or pick another country 
+# filter(country == "Australia") %>%
+
+count_country %>% tabyl(year)
+
 
 # plot count country
 ggplot(count_country, aes(n, country, colour = year))+
@@ -65,6 +83,7 @@ ggplot(count_country, aes(n, country, colour = year))+
 # with a shape?
 ggplot(count_country, aes(n, country, shape = year))+
   geom_point()
+
 
 # Lets look at the companies 
 count_company <- plastics %>%
@@ -122,6 +141,7 @@ volunteers <- plastics %>%
 
 view(volunteers)
 
+
 volunteers %>%
   ggplot(aes(country, volunteers, colour =year))+
   geom_point()+
@@ -158,6 +178,11 @@ volunteers_by_year <-rename(volunteers_by_year, yr2020='2020')
 
 view(volunteers_by_year)
 
+#look at the countries that only participated in one year
+country_participants <- volunteers_by_year %>% 
+  filter(is.na(yr2019) | is.na(yr2020)) 
+
+
 
 volunteers_dif <-volunteers_by_year %>% mutate(difference = (yr2020-yr2019))
 
@@ -190,3 +215,19 @@ volunteers_dif %>%
   ggplot(aes(x=difference, y=reorder(country,-difference))) +
    geom_point() +
    ggtitle("Countrys with a decrease in volunteers from 2019-2020")
+
+
+stats <- plastics %>% select(country, year, num_events, volunteers)%>%
+  distinct(country, year, num_events, volunteers) %>%
+  arrange(country)
+
+ggplot(stats, aes(country, volunteers, fill = year)) +
+  geom_bar(position="dodge2", stat = "identity", width = 0.7)+
+  coord_flip()
+
+  
+
+
+
+
+
